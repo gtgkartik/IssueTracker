9014a0fcb17ed26d7f00e3ba702c89ea22a4a536
kartik kumar <gtgkartik@gmail.com>
2023-12-07 20:14:49 +0530

    done with handle submit form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,16 +2,49 @@
 import { Button, TextField } from "@radix-ui/themes";
 import { TextArea } from "@radix-ui/themes"
 import React from "react";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import { useForm, SubmitHandler, Controller, } from "react-hook-form"
+import axios from 'axios';
+import { useRouter } from 'next/navigation'
+
+type IssueForm = {
+    title: string
+    description: string
+  }
+
 const NewIssue = () => {
+  const router = useRouter();
+    const {
+        control,
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm<IssueForm>()
   return (
     <>
-      <div className="max-w-xl space-y-3">
+      <form 
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+       await axios.post('/api/issues' , data);
+       router.push('/issues')
+      })}
+      
+      >
         <TextField.Root>
-          <TextField.Input placeholder="Title" />
+          <TextField.Input placeholder="Title" {...register("title", { required: true })} />
         </TextField.Root>
-        <TextArea placeholder="Description" />
-        <Button>Submit New Issue</Button>
-      </div>
+        {errors.title && <span className="text-red-600">This field is required</span>}
+
+        <Controller
+        name="description"
+        control={control}
+        render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
+        />
+        
+        {errors.description && <span>This field is required</span>}
+        <Button >Submit New Issue</Button>
+        </form>
     </>
   );
 };
