ae4d81339b6c3d240716be9dc25dc44904574a67
kartik kumar <gtgkartik@gmail.com>
2023-12-07 16:15:16 +0530

    added classnames lib and styles navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,13 @@
+"use client";
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { FaBug } from "react-icons/fa";
+import classnames from 'classnames';
 
 export default function Navbar() {
+ const currentPath =  usePathname();
+
   const Navlinks = [
     {href: "/",  label: "Dashboard"},
     {href: "/issues", label: "Issues"}
@@ -15,8 +20,16 @@ export default function Navbar() {
           <Link href="/"><FaBug size={20}/></Link>
 
           {Navlinks.map((item) => 
-            <Link href={item.href} key={item.href}>
-              <p className='text-zinc-500 hover:text-zinc-800 transition-colors'>{item.label}</p>
+            <Link 
+            href={item.href} 
+            key={item.href}>
+            <p className= {classnames({
+              'text-zinc-900': currentPath === item.href,
+              'text-zinc-500': currentPath !== item.href,
+              'transition-colors hover:text-zinc-800 ': true,
+
+            })}
+            >{item.label}</p>
             </Link>
           )}
           
