60ef1cba7a946e5fbff2e77542889fcdb7ee8789
kartik kumar <gtgkartik@gmail.com>
2023-12-07 18:30:27 +0530

    Add link to create new issue button and update main container padding

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Button } from '@radix-ui/themes'
+import Link from 'next/link'
+
 const IssuesPage = () => {
   return (
     <>
       <h1>Issues</h1>
-      <Button>Button</Button>
+      <Button><Link href="/issues/new ">Button</Link></Button>
     </>
   )
 }
